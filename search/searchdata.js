30af8c0df452ebebd39f2dec1a62d642c1e0a714
tartem22 <tartem22@users.noreply.github.com>
2022-05-11 21:29:10 +0000

    deploy: c731fe0da43399a36a283c5b9cc58080cc7b047e

---
+++ search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "_acdhilmpst",
+  1: "cilmpt",
+  2: "mp",
+  3: "i",
+  4: "_acdhps"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "variables",
+  4: "defines"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions",
+  3: "Variables",
+  4: "Macros"
+};
+
